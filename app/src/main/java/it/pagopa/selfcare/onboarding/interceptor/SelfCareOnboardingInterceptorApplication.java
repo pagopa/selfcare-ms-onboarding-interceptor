f657e897786a8c9e9b36d26f469d81762e4d2051
bgalgamu <kevin.si1396@gmail.com>
2023-02-20 09:48:14 +0100

    App name refactor

--- app/src/main/java/it/pagopa/selfcare/onboarding/interceptor/SelfCareOnboardingInterceptorApplication.java
+++ app/src/main/java/it/pagopa/selfcare/onboarding/interceptor/SelfCareOnboardingInterceptorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SelfCareOnboardingInterceptor {
+public class SelfCareOnboardingInterceptorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SelfCareOnboardingInterceptor.class, args);
+        SpringApplication.run(SelfCareOnboardingInterceptorApplication.class, args);
     }
 
 }
