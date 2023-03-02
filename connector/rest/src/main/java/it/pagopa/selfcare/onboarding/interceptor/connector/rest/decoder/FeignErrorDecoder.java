e3646b773b4e826fdcf93b916cba99b7de8ceff6
bgalgamu <kevin.si1396@gmail.com>
2023-03-02 18:08:47 +0100

    correct decoder error handling

--- connector/rest/src/main/java/it/pagopa/selfcare/onboarding/interceptor/connector/rest/decoder/FeignErrorDecoder.java
+++ connector/rest/src/main/java/it/pagopa/selfcare/onboarding/interceptor/connector/rest/decoder/FeignErrorDecoder.java
@@ -7,7 +7,7 @@ import it.pagopa.selfcare.onboarding.interceptor.exception.InstitutionAlreadyOnb
 public class FeignErrorDecoder extends ErrorDecoder.Default {
     @Override
     public Exception decode(String methodKey, Response response) {
-        if (response.status() == 404) {
+        if (response.status() == 409) {
             throw new InstitutionAlreadyOnboardedException();
         } else {
             return super.decode(methodKey, response);
