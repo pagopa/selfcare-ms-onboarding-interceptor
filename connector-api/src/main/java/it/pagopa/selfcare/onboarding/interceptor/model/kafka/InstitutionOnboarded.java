fe4bc9b6f071369273e3b751fabcd6fa7409b6e7
Kevin Silva <84286727+KevinSi96@users.noreply.github.com>
2023-11-15 17:11:59 +0100

    add category field to notification object read from queue (#48)

--- connector-api/src/main/java/it/pagopa/selfcare/onboarding/interceptor/model/kafka/InstitutionOnboarded.java
+++ connector-api/src/main/java/it/pagopa/selfcare/onboarding/interceptor/model/kafka/InstitutionOnboarded.java
@@ -1,6 +1,7 @@
 package it.pagopa.selfcare.onboarding.interceptor.model.kafka;
 
 import it.pagopa.selfcare.commons.base.utils.InstitutionType;
+import it.pagopa.selfcare.onboarding.interceptor.model.institution.RootParent;
 import lombok.Data;
 
 @Data
@@ -12,4 +13,13 @@ public class InstitutionOnboarded {
     private String taxCode;
     private String origin;
     private String originId;
+    private String zipCode;
+    private String istatCode;
+    private String city;
+    private String country;
+    private String county;
+    private String subUnitCode;
+    private String category;
+    private String subUnitType;
+    private RootParent rootParent;
 }
